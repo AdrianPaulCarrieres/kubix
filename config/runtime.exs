cfdc67809bbeb7e2410f2a5cb2533d0753b72cb4
Adrian-Paul Carrières <36892477+AdrianPaulCarrieres@users.noreply.github.com>
2022-12-01 17:56:05 +0100

    fighting against host

--- config/runtime.exs
+++ config/runtime.exs
@@ -58,7 +58,8 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base,
+    check_origin: :conn
 
   # ## SSL Support
   #
